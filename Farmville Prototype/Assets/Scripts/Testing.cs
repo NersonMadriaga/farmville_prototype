44b63138758ad2776c37f51faa4b3d7fa5a718b5
Nerson <nersonmadriaga.taktyl@gmail.com>
2021-02-13 13:02:16 +0800

    chore: Added new assets
    Added new Assets
    Added new Script for GridTerrain.

---
+++ Farmville Prototype/Assets/Scripts/Testing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] int columns, rows;
+    [SerializeField] float cellSize, xOrigin, yOrigin;
+    private GridTerrain grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new GridTerrain(columns,rows,cellSize,new Vector3(xOrigin,yOrigin));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(),56);
+        }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+
+
+    }
+}
